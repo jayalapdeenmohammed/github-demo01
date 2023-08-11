8f86652513a42f13368545eaefbccf946dc172a6
jayalapdeenmohammed <jayalapdeen@gmail.com>
2023-08-11 12:10:44 +0530

    third commit

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ Created on Aug 11, 2023
 @author: JAYALAPDEEN
 '''
 # This is sample file
+from IPython.utils.tests.test_pycolorize import sample
 print("GIT File folder directory")
 
-Test
+Test sample
+
+sample data (dfsdgdfgdfgdsf)
